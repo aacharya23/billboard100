4853c6361da4436d80afa606284019b51ded0fc3
aacharya23 <aacharya@smith.edu>
2022-11-17 10:32:23 -0500

    added documentation

--- R/clean_function.R
+++ R/clean_function.R
@@ -1,11 +1,34 @@
-BB100<-charts %>%
-  select( -rank,-date,-`last-week`,-`weeks-on-board`,-`peak-rank`) %>%
-  group_by(artist) %>%
-  summarize(count=n()) %>%
-  arrange(desc(count))
+#' Find top x songs of all time
+#'
+#'
+#'
+#' @param data the billboard dataset of all top songs from all time used to calculate the top x number of songs
+#' @param x the number that the user inputs to find the top number of songs
+#'
+#' @return 'TopN_BB100' returns a dataframe with x number of rows of top songs over time based on the user input
+#'
+#' @examples
+#' library(billboard_100)
+#'
+#' # Find the top 10 songs of all time
+#' TopN_BB100(data=BB100, 10)
+#'
+#' # Find the top 20 songs of all time
+#' TopN_BB100(data= BB100, 20)
+#'
+#' @importFrom magrittr "%>%"
+#' @import dplyr
+#' @export
+
+
 
 TopN_BB100 <- function(data = Top_BB100, x) {
   return(top_n(data, n = (x), wt = count))
 }
 
-TopN_BB100(data=BB100, 11)
+BB100<-charts %>%
+  select(-c(rank,date,`last-week`,`weeks-on-board`,`peak-rank`)) %>%
+  group_by(artist) %>%
+  summarize(count=n()) %>%
+  dplyr::arrange(desc(count))
+
