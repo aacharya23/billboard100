facca7e9e40510a1f30e32fb943ff60c2c73a54f
Lillian Fok <lfok@smith.edu>
2022-11-22 16:18:00 -0500

    test edits

--- tests/testthat/test-top-n.R
+++ tests/testthat/test-top-n.R
@@ -1,9 +1,14 @@
+top100 <- top100 %>%
+  dplyr::select(song,artist) %>%
+  dplyr::group_by(artist) %>%
+  dplyr::summarize(count=n()) %>%
+  dplyr::arrange(desc(count))
 
 test_that("top n function",
           {
             expect_equal(
               billboard100::top_n_bb100(x = 5),
-              top_n(top100, n = 5, wt = song)
+              top_n(top100, n = 5, wt = count)
             )
           }
 )
