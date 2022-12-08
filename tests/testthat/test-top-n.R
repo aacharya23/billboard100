67982e8d7ac4314511863fe45493e7b5d2acc706
Lillian Fok <lfok@smith.edu>
2022-12-08 10:33:38 -0500

    added test for top_n function

--- tests/testthat/test-top-n.R
+++ tests/testthat/test-top-n.R
@@ -1,9 +1,16 @@
+library(tidyverse)
+
+expected_df <- tibble(
+  song = c("Stay", "Angel"),
+  count = c(224, 205)
+)
 
 test_that("top n function",
           {
             expect_equal(
-              billboard100::top_n_bb100(x = 1)$artist,
-              "Taylor Swift"
+              billboard100::top_n(id = "song", n = 2), expected_df
             )
           }
+
+
 )
