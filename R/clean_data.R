6e78560a2526166d9f0a5b329dc51c9a7a648860
aacharya23 <aacharya@smith.edu>
2022-12-14 22:36:38 -0500

    explicit function

--- R/clean_data.R
+++ R/clean_data.R
@@ -10,17 +10,16 @@
 #' library(billboard100)
 #'
 #' # Clean any music dataset
-#' clean_data(data)
+#' clean_data(top100)
 #'
 #'
 #' @importFrom stringi stri_trans_general
 #' @export
 
 clean_data <- function(data = top100) {
-top100$artist <- stri_trans_general(top100$artist, "latin-ascii")
-top100$song <- stri_trans_general(top100$song, "latin-ascii")
+  top100$artist <- stri_trans_general(top100$artist, "latin-ascii")
+  top100$song <- stri_trans_general(top100$song, "latin-ascii")
 
-
-return(top100)
+  return(top100)
 }
 
